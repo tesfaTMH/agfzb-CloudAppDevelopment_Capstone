429c0ffd0839df4292f6cf75a9fcc1e1393dda67
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2023-12-11 19:01:42 +0200

    minor modifications and developing the app based on pytho3.8 version

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 APPEND_SLASH = True
 
 #ALLOWED_HOSTS = ["localhost"]
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost",
+                 "127.0.0.1"]
 
 
 # Application definition
